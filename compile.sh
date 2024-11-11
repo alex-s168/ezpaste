7c087357c9a0559f5e3480f41c4e2110d3f69941
alex-s168 <alexandernutz68@gmail.com>
2024-11-11 21:12:54 +0100

    fixed

--- compile.sh
+++ compile.sh
@@ -1,13 +1,15 @@
-http_serv=""
-http_serv+="c-http-server/src/*.c c-http-server/C-Thread-Pool/thpool.c"
-http_serv+=" -DHAS_ZSTD -lzstd"
-# TODO: broken for absolutely no reason
-#http_serv+=" -DHAS_ZLIB -lz"
-
-app=""
-app+="main.c utils.c memmap.c templating_lang.c"
-app+=" -lsparkey"
-
 : ${CC:="clang"}
+: ${CXX:="clang++"}
+: ${CFLAGS:="-flto -g -ggdb"}
+: ${LDFLAGS:=""}
 
-$CC $app $http_serv -o out -O0 -g -glldb
+http_serv="c-http-server/src/*.c c-http-server/C-Thread-Pool/thpool.c"
+app="main.c utils.c memmap.c templating_lang.c"
+link="-lsparkey -lpthread -lsnappy -lzstd"
+
+mkdir -p obj
+for file in $app $http_serv; do
+    repl=${file//\//_}
+    ${CC} -DHAS_ZSTD -c $file -o "obj/$repl.o" $CFLAGS
+done
+${CXX} obj/*.o -o out $link $LDFLAGS
